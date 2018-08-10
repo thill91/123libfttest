725037eb8b40bf1601d4e87ea2a942e27567d641
Tyler Hill <thill@e1z2r6p8.42.us.org>
2018-08-09 22:00:49 -0700

    s

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,39 +6,39 @@
 /*   By: adorn <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 09:10:43 by adorn             #+#    #+#             */
-/*   Updated: 2016/11/10 11:18:37 by adorn            ###   ########.fr       */
+/*   Updated: 2018/08/09 21:57:21 by thill            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int		find_wd_count(const char *s, char d)
+static int		wd_count(const char *s, char d)
 {
-	int			position;
-	int			nb;
+	int			index;
+	int			n;
 
-	position = 0;
-	nb = 0;
-	while (*s != '\0')
+	index = 0;
+	n = 0;
+	while (*s)
 	{
-		if (*s != d && position == 0)
+		if (*s != d && index == 0)
 		{
-			position = 1;
-			nb++;
+			index = 1;
+			n++;
 		}
-		if (*s == d && position == 1)
-			position = 0;
+		if (*s == d && index == 1)
+			index = 0;
 		s++;
 	}
-	return (nb);
+	return (n);
 }
 
-static int		find_wd_len(const char *s, char d)
+static int		wd_len(const char *s, char d)
 {
-	int			len;
+	int	len;
 
 	len = 0;
-	while (*s != '\0' && *s != d)
+	while (*s && *s != d)
 	{
 		len++;
 		s++;
@@ -48,28 +48,28 @@ static int		find_wd_len(const char *s, char d)
 
 char			**ft_strsplit(char const *s, char c)
 {
-	char		**str;
+	char		**t;
 	int			wd;
 	int			i;
 
 	i = 0;
 	if (!s)
 		return (0);
-	wd = find_wd_count(s, c);
-	str = (char**)malloc(sizeof(*str) * (find_wd_count(s, c) + 1));
-	if (str == NULL)
+	wd = wd_count(s, c);
+	t = (char**)malloc(sizeof(*t) * (wd_count(s, c) + 1));
+	if (t == NULL)
 		return (NULL);
 	while (wd > 0)
 	{
-		while (*s == c && *s != '\0')
+		while (*s == c && *s)
 			s++;
-		str[i] = ft_strsub(s, 0, find_wd_len(s, c));
-		if (str[i] == NULL)
+		t[i] = ft_strsub(s, 0, wd_len(s, c));
+		if (t[i] == NULL)
 			return (NULL);
-		s = s + find_wd_len(s, c);
+		s = s + wd_len(s, c);
 		i++;
 		wd--;
 	}
-	str[i] = NULL;
-	return (str);
+	t[i] = NULL;
+	return (t);
 }
