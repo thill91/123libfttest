07ab7923cae2aeef6ea977e2fe7aebd3d1d95b3a
Tyler Hill <thill@e1z2r6p8.42.us.org>
2018-08-09 21:28:23 -0700

    d

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -3,38 +3,73 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strsplit.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thill <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: adorn <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/08 14:34:30 by thill             #+#    #+#             */
-/*   Updated: 2018/08/09 16:12:01 by thill            ###   ########.fr       */
+/*   Created: 2016/11/10 09:10:43 by adorn             #+#    #+#             */
+/*   Updated: 2016/11/10 11:18:37 by adorn            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_strsplit(const char *s, char c)
+static int		find_wd_count(const char *s, char d)
 {
-	size_t	i;
-	size_t	j;
-	size_t	k;
-	char	**t;
+	int			position;
+	int			nb;
+
+	position = 0;
+	nb = 0;
+	while (*s != '\0')
+	{
+		if (*s != d && position == 0)
+		{
+			position = 1;
+			nb++;
+		}
+		if (*s == d && position == 1)
+			position = 0;
+		s++;
+	}
+	return (nb);
+}
+
+static int		find_wd_len(const char *s, char d)
+{
+	int			len;
+
+	len = 0;
+	while (*s != '\0' && *s != d)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+char			**ft_strsplit(char const *s, char c)
+{
+	char		**str;
+	int			wd;
+	int			i;
 
 	i = 0;
-	k = 0;
-	if (!s || !(t = (char **)malloc(sizeof(char *) * (ft_wcount(s, c) + 1))))
+	if (!s)
+		return (0);
+	wd = find_wd_count(s, c);
+	str = (char**)malloc(sizeof(*str) * (find_wd_count(s, c) + 1));
+	if (str == NULL)
 		return (NULL);
-	while (i < ft_wcount(s, c))
+	while (wd > 0)
 	{
-		if (!(t[i] = (char *)malloc(sizeof(char *) * (ft_wlen(&s[k], c) + 1))))
+		while (*s == c && *s != '\0')
+			s++;
+		str[i] = ft_strsub(s, 0, find_wd_len(s, c));
+		if (str[i] == NULL)
 			return (NULL);
-		j = 0;
-		while (s[k] == c)
-			k += 1;
-		while (s[k] != c && s[k] != '\0')
-			t[i][j++] = s[k++];
-		t[i][j] = '\0';
-		i += 1;
+		s = s + find_wd_len(s, c);
+		i++;
+		wd--;
 	}
-	t[i] = NULL;
-	return (t);
+	str[i] = NULL;
+	return (str);
 }
